088452db3a08191d2b197eb3a3e6f9a260c5dd9c
René <rene.fuehrer@generali.com>
2019-02-08 18:51:07 +0100

    added more options

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ measurement_interval=30
 transmission_interval=3600
 anomaly_detection_difference = 2
 low_power_consumption_mode = 0
-
+send_all_data = 1
+fast_boot = 1
 
 # ################################################################
 # ########   main
@@ -46,14 +47,16 @@ sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
 sigfox_network = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
 sigfox_network.setblocking(True)
 sigfox_network.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
+device_id = binascii.hexlify(sigfox.id())
+device_pac = binascii.hexlify(sigfox.pac())
 
 # INIT
 # fake green status
 # test uplink/downlink - if successful, send green light, else red light
-pycom.rgbled(0x007f00)
-time.sleep(5)
-pycom.rgbled(0x000000)
-
+if fast_boot == 0:
+    pycom.rgbled(0x007f00)
+    time.sleep(5)
+    pycom.rgbled(0x000000)
 
 this_interval=0
 init_count=0
@@ -66,8 +69,8 @@ while True:
     if low_power_consumption_mode == 0:
         print("measuring...")
         pycom.rgbled(0x00007f)
-    this_temperature=MPL3115A2(py,mode=ALTITUDE).temperature()
-    this_temperature = int(this_temperature*2+80)
+    original_temperature=MPL3115A2(py,mode=ALTITUDE).temperature()
+    now_temperature = int(original_temperature*2+80)
     if low_power_consumption_mode == 0:
         pycom.rgbled(0x000000)
 
@@ -77,32 +80,40 @@ while True:
         #print("transmission_interval=%s" % transmission_interval)
         #print("intervals=%s" % intervals)
         #print("this_interval=%s" % str(this_interval))
-        print("temperature (this) : %s ((temp-80)/2)" % this_temperature)
-        print("temperature (old)  : %s ((temp-80)/2)"  % old_temperature)
-        print("temperature anomaly: %s (>=)" % anomaly_detection_difference)
+        print("temperature (this) [%s]: %s ((temp-80)/2=%s)" % (device_id, now_temperature, original_temperature))
+        print("temperature (old)  [%s]: %s ((temp-80)/2)"  % (device_id, old_temperature))
+        print("temperature anomaly[%s]: %s (>=)" % (device_id, anomaly_detection_difference))
+
+    if init_count == 0:
+        # first start => send message
+            if low_power_consumption_mode == 0:
+                print("sending first value after restart... (green:%s,%s)" % (now_temperature, old_temperature))
+            pycom.rgbled(0x007f00)
+            sigfox_network.send(bytes([now_temperature, old_temperature]))
+            pycom.rgbled(0x000000)
 
     if init_count == 1:
         # only if first measurement completed
-        if this_temperature >= (old_temperature + anomaly_detection_difference):
+        if now_temperature >= (old_temperature + anomaly_detection_difference):
             if low_power_consumption_mode == 0:
-                print("sending alarm... (red:%s,%s)" % (this_temperature, old_temperature))
+                print("sending alarm... (red:%s,%s)" % (now_temperature, old_temperature))
             
             pycom.rgbled(0x7f0000)
-            sigfox_network.send(bytes([this_temperature,old_temperature]))
+            sigfox_network.send(bytes([now_temperature,old_temperature]))
             pycom.rgbled(0x000000)
             this_interval=0
             already_sent=1
 
-    old_temperature=this_temperature
+    old_temperature=now_temperature
 
     if already_sent == 0:
-        # onlys end if not already red status
-        if intervals == 1.0:
+        # only end if not already red status
+        if (intervals == 1.0) or (send_all_data == 1):
             if low_power_consumption_mode == 0:
-                print("sending... (green:%s,%s)" % (this_temperature,old_temperature))
+                print("sending... (green:%s,%s)" % (now_temperature,old_temperature))
             pycom.rgbled(0x007f00)
-            sigfox_network.send(bytes([this_temperature, old_temperature]))
-            #pybytes.send_virtual_pin_value(False,15,int(this_temperature))
+            sigfox_network.send(bytes([now_temperature, old_temperature]))
+            #pybytes.send_virtual_pin_value(False,15,int(now_temperature))
             this_interval=0
             pycom.rgbled(0x000000)
 
