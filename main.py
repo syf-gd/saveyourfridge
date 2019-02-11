5d47bade4438335d10658a972d2d256871309a2e
René <rene.fuehrer@generali.com>
2019-02-11 22:17:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,28 @@
 # green (<5sec) =   sending alarm
 
 # ################################################################
+# ########   Variables
+# ################################################################
+measurement_interval=30             # #=seconds a measurement will be done (30=>5 minutes)
+transmission_interval=3600          # #=seconds a message will be sent (independently of alarm) (3600=>15 minutes)
+anomaly_detection_difference = 2    # #=differences in degrees(celsius) to send alarm by device
+low_power_consumption_mode = 0      # 1=send device to deep sleep mode (attention: system is not connectable anymore)
+send_all_data = 1                   # 1=send every measurement
+fast_boot = 1                       # 1=no signal strength test at boot
+protocol_version=1                  # #=1-254 (change, if data format changed)
+
+# protocol versions:
+# (1)   initial version
+#       AABB; AA=protocaol version, BB=temperature
+
+# ################################################################
+# ################################################################
+# ########   +++++ NO CHANGES BELOW THIS LINE +++++
+# ################################################################
+# ################################################################
+
+
+# ################################################################
 # ########   imports
 # ################################################################
 import sys
@@ -24,14 +46,6 @@ import gc
 import time
 
 py = Pysense()
-
-# messen alle 5 minuten, senden alle 15 minuten
-measurement_interval=30
-transmission_interval=3600
-anomaly_detection_difference = 2
-low_power_consumption_mode = 0
-send_all_data = 1
-fast_boot = 1
 
 # ################################################################
 # ########   main
@@ -49,8 +63,12 @@ sigfox_network.setblocking(True)
 sigfox_network.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 device_id = binascii.hexlify(sigfox.id())
 device_pac = binascii.hexlify(sigfox.pac())
+print("DEVICE ID : %s" % (device_id))
+print("DEVICE PAC: %s" % (device_pac))
 
-# INIT
+# ################################################################
+# ########   pre-check
+# ################################################################
 # fake green status
 # test uplink/downlink - if successful, send green light, else red light
 if fast_boot == 0:
@@ -58,6 +76,9 @@ if fast_boot == 0:
     time.sleep(5)
     pycom.rgbled(0x000000)
 
+# ################################################################
+# ########   measurement loop
+# ################################################################
 this_interval=0
 init_count=0
 old_temperature=0
@@ -87,19 +108,21 @@ while True:
     if init_count == 0:
         # first start => send message
             if low_power_consumption_mode == 0:
-                print("sending first value after restart... (green:%s,%s)" % (now_temperature, old_temperature))
-            pycom.rgbled(0x007f00)
-            sigfox_network.send(bytes([now_temperature, old_temperature]))
-            pycom.rgbled(0x000000)
+                print("sending first value after restart... (green:%s; v:%s)" % (now_temperature, old_temperature, protocol_version))
+            if low_power_consumption_mode == 0:
+                pycom.rgbled(0x007f00)
+            sigfox_network.send(bytes([protocol_version,now_temperature]))
+            if low_power_consumption_mode == 0:
+                pycom.rgbled(0x000000)
 
     if init_count == 1:
         # only if first measurement completed
         if now_temperature >= (old_temperature + anomaly_detection_difference):
             if low_power_consumption_mode == 0:
-                print("sending alarm... (red:%s,%s)" % (now_temperature, old_temperature))
+                print("sending alarm... (red:%s;v:%s)" % (now_temperature, protocol_version))
             
             pycom.rgbled(0x7f0000)
-            sigfox_network.send(bytes([now_temperature,old_temperature]))
+            sigfox_network.send(bytes([protocol_version,now_temperature,old_temperature]))
             pycom.rgbled(0x000000)
             this_interval=0
             already_sent=1
@@ -110,9 +133,9 @@ while True:
         # only end if not already red status
         if (intervals == 1.0) or (send_all_data == 1):
             if low_power_consumption_mode == 0:
-                print("sending... (green:%s,%s)" % (now_temperature,old_temperature))
+                print("sending... (green:%s;v:%s)" % (now_temperature,protocol_version))
             pycom.rgbled(0x007f00)
-            sigfox_network.send(bytes([now_temperature, old_temperature]))
+            sigfox_network.send(bytes([protocol_version,now_temperature]))
             #pybytes.send_virtual_pin_value(False,15,int(now_temperature))
             this_interval=0
             pycom.rgbled(0x000000)
